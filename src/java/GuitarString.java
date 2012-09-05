f6ad3d6dadb4e7aa38322dd2dec2863b51bc9b32
c0ndu17 <georgephillips22@gmail.com>
2012-09-05 09:05:06 +0200

    HomeUpdate

---
+++ src/java/GuitarString.java
@@ -0,0 +1,26 @@
+public class GuitarString{
+//------------------------------------------------------------------------------------------------------------------------
+	GuitarString(double frequency){  // create a guitar string of the given frequency, using a sampling rate of 44,100
+
+	}
+
+     	GuitarString(double[] init){     // create a guitar string whose size and initial values are given by the array
+
+	}
+
+  	void pluck(){                         // set the buffer to white noise
+
+	}
+
+  	void tic(){                           // advance the simulation one time step
+
+	}
+
+	double sample(){                       // return the current sample
+
+	}
+
+   	int time(){                          // return number of tics
+
+	}
+}
